2bde4c7dbf1b4f92d0414ea97ade8fe51aa93781
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-06-10 05:17:51 +0200

    Added MainViewModelTests class; Created ctor;

--- Horse_Picker.Tests/ViewModels/MainViewModelTests.cs
+++ Horse_Picker.Tests/ViewModels/MainViewModelTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Horse_Picker.DataProvider;
+using Horse_Picker.Dialogs;
+using Horse_Picker.ViewModels;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +10,17 @@ namespace Horse_Picker.Tests.ViewModels
 {
     public class MainViewModelTests
     {
+        private MainViewModel _viewModel;
+        private Mock<IMessageDialogService> _messageDialogServiceMock;
+        private Mock<IFileDataServices> _dataServices;
+        private Mock<IScrapDataServices> _scrapServices;
 
+        public MainViewModelTests()
+        {
+            _messageDialogServiceMock = new Mock<IMessageDialogService>();
+            _dataServices = new Mock<IFileDataServices>();
+            _scrapServices = new Mock<IScrapDataServices>();
+            _viewModel = new MainViewModel(_dataServices.Object, _scrapServices.Object, _messageDialogServiceMock.Object);
+        }
     }
 }
