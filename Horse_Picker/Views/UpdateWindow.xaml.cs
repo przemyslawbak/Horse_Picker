7aa2bc56d91fe5f8b4d340eb30291b7b0cea62c0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-06-18 09:44:44 +0200

    Updated dialog view namespaces;

--- Horse_Picker/Views/UpdateWindow.xaml.cs
+++ Horse_Picker/Views/UpdateWindow.xaml.cs
@@ -1,21 +1,4 @@
-﻿using Autofac;
-using Horse_Picker.DataProvider;
-using Horse_Picker.Dialogs;
-using Horse_Picker.Startup;
-using Horse_Picker.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Horse_Picker.Views
 {
